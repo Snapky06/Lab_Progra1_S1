667712512560fed71a47249da37941912cb7b418
Snapky06 <saidnapky@gmail.com>
2025-05-09 11:53:27 -0600

    terminado

--- Napky_Rajo1.java/src/Semana_3/Napky_Said_Estructura.java
+++ Napky_Rajo1.java/src/Semana_3/Napky_Said_Estructura.java
@@ -13,19 +13,30 @@ import java.util.Scanner;
 public class Napky_Said_Estructura {
 
     public static void main(String[] args) {
-        System.out.println("1- Palabra Alreves");
-        System.out.println("2-Numero Perfecto");
-        System.out.println("3- Primos");
-        System.out.println("4- Votaciones");
-        System.out.print("Seleccione (1 O 2 O 3 O 4) : ");
+
+        int cantidad = 0, largo = 0, numero = 0, sumaDivisores = 0, divisor_primo = 0;
+        int cuentaDivisores = 0,codigo=0,voto_total=0;
+        
+        double porcentaje_ganador = 0.0;
+        String palabra = "", invertida = "", palabra2 = "",palabramax="";
+        boolean esPerfecto = false, esPrimo = false;
+       int cantidad_azul = 0, cantidad_amarillo = 0, cantidad_rojo = 0, cantidad_negro = 0, cantidad_nulo = 0;
+        int opcion1=0, opcion2=0 , opcion3=0, opcion4=0,palabra4=0,palabracompara=0;
+        
+        do {
+                System.out.println("1- Palabra Alreves");
+                System.out.println("2-Numero Perfecto");
+                System.out.println("3- Primos");
+                System.out.println("4- Votaciones");
+                System.out.println("5-Salir");
+                System.out.print("Seleccione (1,2,3,4 O 5) : ");
 
         Scanner leer = new Scanner(System.in);
-        int cantidad = 0, largo = 0, numero = 0, sumaDivisores = 0, divisor_primo = 0;
-        int codigo = leer.nextInt(), cuentaDivisores = 0;
-        String palabra = "", invertida = "", palabra2 = "";
-        boolean esPerfecto = false, esPrimo = false;
-        switch (codigo) {
+        
+        codigo = leer.nextInt();
+            switch (codigo) {
             case 1:
+                opcion1++;
                 System.out.println("Ingrese La Cantidad De Palabras");
                 cantidad = leer.nextInt();
 
@@ -36,6 +47,11 @@ public class Napky_Said_Estructura {
                     palabra = leer.next();
 
                     largo = palabra.length();
+                    
+                    if(largo>palabra4){
+                    palabra4 = largo;
+                    palabramax = palabra;
+                    }
 
                     for (int i = largo - 1; i >= 0; i--) {
                         invertida += palabra.charAt(i);
@@ -46,14 +62,16 @@ public class Napky_Said_Estructura {
                     palabra2 = invertida;
 
                     if (palabra.equals(palabra2)) {
-                        System.out.print("Es Palindromo");
+                        System.out.println("Es Palindromo");
                     }
+                    invertida="";
 
                 }
-
+                    System.out.println("La Palabra Mas Grande Es : " + palabramax);
                 break;
 
             case 2:
+                opcion2++;
                 System.out.println("Ingrese Un Numero : ");
 
                 numero = leer.nextInt();
@@ -73,7 +91,7 @@ public class Napky_Said_Estructura {
                 break;
 
             case 3:
-
+opcion3++;
                 int Numero_Random = (int) (Math.random() * 100) + 1;
                 esPrimo = true;
 
@@ -94,6 +112,73 @@ public class Napky_Said_Estructura {
 
                 break;
 
+            case 4:
+                opcion4++;
+                System.out.println("Cuantos Votantes Hay En El Pais : ");
+                int cantidad_votantes = leer.nextInt();
+                
+                for(int repeticiones_votos = 0;repeticiones_votos<cantidad_votantes;repeticiones_votos++){
+                    System.out.print("Por Quien Votara Azul,Rojo,Negro o Amarillo: ");
+                    
+                    String voto = leer.next();
+                    
+                    switch(voto){
+                        case "Azul" :
+                            cantidad_azul++;
+                            break;
+                            
+                            case "Rojo" :
+                            
+                                cantidad_rojo ++;
+                                
+                            break;
+                            
+                            case "Negro" :
+                            
+                                cantidad_negro ++;
+                                
+                            break;
+                            
+                            case "Amarillo" :
+                            
+                            cantidad_amarillo ++;
+                                
+                            break;
+                            
+                            default : 
+                                cantidad_nulo++;    
+                                    break;
+                    }
+                    }
+                
+                    
+                    voto_total = cantidad_azul + cantidad_rojo + cantidad_negro + cantidad_amarillo ;
+                    
+                    if(voto_total/cantidad_votantes >= 0.60){
+                        
+                    if(cantidad_azul > cantidad_rojo &&  cantidad_azul > cantidad_negro && cantidad_azul > cantidad_amarillo ){
+                        System.out.println("Gano El Azul ");
+                    } else if(cantidad_rojo > cantidad_azul && cantidad_rojo > cantidad_negro && cantidad_rojo > cantidad_amarillo ){
+                        System.out.println("Gano El Rojo");
+                    }else if(cantidad_amarillo > cantidad_azul && cantidad_amarillo > cantidad_negro && cantidad_amarillo > cantidad_rojo ){
+                        System.out.println("Gano El Amarillo");
+                    }else if(cantidad_negro > cantidad_azul && cantidad_negro > cantidad_rojo && cantidad_negro > cantidad_amarillo ){
+                        System.out.println("Gano El Negro");
+                    }else{
+                        System.out.println("No Hay Claro Ganador");
+                    }
+                    
+                    
+                    }else{
+                        System.out.println("Insuficiente Participacion");
+                    }
+                
         }
+        }while(codigo!=5);
+        
+        System.out.println("Entro " + opcion1 + " A la Primera Opcion");
+        System.out.println("Entro " + opcion2 + " A la Segunda Opcion");
+        System.out.println("Entro " + opcion3 + " A la Tercera Opcion");
+        System.out.println("Entro " + opcion4 + " A la Cuarta Opcion");
     }
 }
